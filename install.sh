b86cb3a7dac3dcbc74f05a7d7d3cc6f75a049d26
rui-carter <gemslove2@gmail.com>
2025-01-10 23:22:02 +0200

    fix: испрвил баг с тем что изображения не показывались

--- install.sh
+++ install.sh
@@ -16,15 +16,23 @@ yes | pacman -Sy kitty make \
     thunar wofi wofi-power-menu code blueman \
     zsh neofetch
 
-
 # ZSH Powerlevel10k
+git clone --depth=1 https://github.com/URLbug/NeofetchRandomizer.git ~/NeofetchRandomizer
+echo "source $HOME/NeofetchRandomizer/random.sh" >> ~/.zshrc
+
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 
-rsync ./.zshrc ~/.zshrc
 rsync ./.p10k.zsh ~/.p10k.zsh
 
 chsh -s $(which zsh)
+
+# Kitty theme and Neofetch
+rm -rf ~/.config/kitty/*
+rm -rf ~/.config/neofetch/*
+
+rsync ./kitty/* ~/.config/kitty/
+rsync ./neofetch/* ~/.config/neofetch/
 
 # Hypr configs
 rm -rf ~/.config/hypr
