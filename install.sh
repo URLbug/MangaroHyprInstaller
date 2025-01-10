67149fdf13c975e759132635de1a0a06d80d1c63
rui-carter <gemslove2@gmail.com>
2025-01-10 22:40:48 +0200

    update: добавил zsh

--- install.sh
+++ install.sh
@@ -13,7 +13,18 @@ yes | pacman -Sy hyprland hypridle hyprpaper
 yes | pacman -Sy kitty make \
     git waybar otf-font-awesome \
     ttf-arimo-nerd noto-fonts \
-    thunar wofi wofi-power-menu code blueman
+    thunar wofi wofi-power-menu code blueman \
+    zsh neofetch
+
+
+# ZSH Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+
+rsync ./.zshrc ~/.zshrc
+rsync ./.p10k.zsh ~/.p10k.zsh
+
+chsh -s $(which zsh)
 
 # Hypr configs
 rm -rf ~/.config/hypr
